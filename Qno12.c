1e37e228c43b662dd2b4a34c1810a17a239ad2b3
Kshitiz-Mhto <nkcmcdhy@gmail.com>
2022-08-26 14:45:19 +0545

    some change in lines

--- Qno12.c
+++ Qno12.c
@@ -4,7 +4,7 @@ int main(){
     int num;
     printf("Enter the number :> ");
     scanf("%d",&num);
-    printf("The factorial of %d is %d.\n",num,fact(num));
+    printf("The factorial at %d th place is %d.\n",num,fact(num));
     return 0;
 }
 int fact( int num){
